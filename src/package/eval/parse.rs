a04f15ece49ce6a47dee526c534412b6cecf7910
CarbonSmasher <crabonsmasher@gmail.com>
2023-02-27 13:14:12 -0500

    Finally a working parser

--- src/package/eval/parse.rs
+++ src/package/eval/parse.rs
@@ -1,23 +1,28 @@
 use std::collections::HashMap;
 
 use crate::io::files::paths::Paths;
-
+use crate::package::eval::lex::Side;
 use super::super::{Package, PkgError};
 use super::lex::{lex, LexError, Token, reduce_tokens, TextPos};
+
+static DEFAULT_ROUTINE: &str = "__default__";
 
 #[derive(Debug, thiserror::Error)]
 pub enum ParseError {
 	#[error("{}", .0)]
 	Lex(#[from] LexError),
 	#[error("Unexpected token '{}' at {}", .0, .1)]
-	UnexpectedToken(String, TextPos)
+	UnexpectedToken(String, TextPos),
+	#[error("Routines must be declared at the root scope {}", .0)]
+	UnexpectedRoutine(TextPos),
+	#[error("Unknown instruction '{}' at {}", .0, .1)]
+	UnknownInstr(String, TextPos)
 }
 
 #[derive(Debug, Clone)]
 pub enum InstrKind {
-	None,
-	Routine(String),
-	If
+	Unknown(String, Vec<String>),
+	If(BlockId)
 }
 
 #[derive(Debug, Clone)]
@@ -26,55 +31,111 @@ pub struct Instruction {
 }
 
 impl Instruction {
-	pub fn new() -> Self {
-		Self {
-			kind: InstrKind::None
-		}
-	}
-
-	pub fn new_kind(kind: InstrKind) -> Self {
+	pub fn new(kind: InstrKind) -> Self {
 		Self {
 			kind
 		}
 	}
 }
 
-// Data for the root of an instruction
-pub enum ParseRoot {
-	Instruction(String, Vec<String>),
-	Routine(String),
-	None
+type BlockId = u16;
+
+#[derive(Debug, Clone)]
+pub struct InstrBlock {
+	contents: Vec<Instruction>,
+	parent: Option<BlockId>
+}
+
+impl InstrBlock {
+	pub fn new(parent: Option<BlockId>) -> Self {
+		Self {
+			contents: Vec::new(),
+			parent
+		}
+	}
+
+	pub fn push(&mut self, instr: Instruction) {
+		self.contents.push(instr);
+	}
+}
+
+#[derive(Debug)]
+pub struct Parsed {
+	pub blocks: HashMap<BlockId, InstrBlock>,
+	pub routines: HashMap<String, BlockId>,
+	id_count: BlockId
+}
+
+impl Parsed {
+	pub fn new() -> Self {
+		let mut out = Self {
+			blocks: HashMap::new(),
+			routines: HashMap::new(),
+			id_count: 0
+		};
+		out.routines = HashMap::from([(String::from(DEFAULT_ROUTINE), out.new_block(None))]);
+		out
+	}
+
+	// Creates a new block and returns its ID
+	pub fn new_block(&mut self, parent: Option<BlockId>) -> BlockId {
+		self.id_count += 1;
+		self.blocks.insert(self.id_count, InstrBlock::new(parent));
+		self.id_count
+	}
+
+	// Creates a new routine and its associated block, then returns the block's ID
+	pub fn new_routine(&mut self, name: &str) -> BlockId {
+		self.new_block(None);
+		self.routines.insert(name.to_owned(), self.id_count);
+		self.id_count
+	}
+}
+
+// Mode for what we are currently parsing
+#[derive(Debug)]
+enum ParseMode {
+	Root,
+	Routine(Option<String>),
+	Instruction(Instruction, Vec<String>),
+	If(Vec<String>)
 }
 
 // Data used for parsing
 #[derive(Debug)]
 pub struct ParseData {
-	routines: HashMap<String, Vec<Instruction>>,
+	parsed: Parsed,
 	instruction_n: u32,
-	routine: String,
-	block: Vec<Instruction>,
-	instruction: Instruction,
-	push_to_routine: bool
+	block: BlockId,
+	mode: ParseMode
 }
 
 impl ParseData {
 	pub fn new() -> Self {
-		let default_rtn = String::from("__default__");
 		Self {
-			routines: HashMap::from([(default_rtn.clone(), Vec::new())]),
+			parsed: Parsed::new(),
 			instruction_n: 0,
-			routine: default_rtn,
-			block: Vec::new(),
-			instruction: Instruction::new(),
-			push_to_routine: false
+			block: 1,
+			mode: ParseMode::Root
 		}
 	}
 
-	// Reset the instruction data
-	pub fn reset_instruction(&mut self) {
+	// Push a new instruction to the block
+	pub fn new_instruction(&mut self, instr: Instruction) {
 		self.instruction_n += 1;
-		self.block.push(self.instruction.clone());
-		self.instruction = Instruction::new();
+		if let Some(block) = self.parsed.blocks.get_mut(&self.block) {
+			block.push(instr);
+		}
+		self.mode = ParseMode::Root;
+	}
+
+	// Finish the current block
+	pub fn new_block(&mut self) {
+		if let Some(block) = self.parsed.blocks.get_mut(&self.block) {
+			if let Some(parent) = block.parent {
+				self.block = parent;
+			}
+		}
 	}
 }
 
@@ -90,37 +151,134 @@ impl Package {
 
 			let mut prs = ParseData::new();
 			for (tok, pos) in tokens.iter() {
-				let mut instr_finished = false;
+				let mut instr_to_push = None;
+				let mut mode_to_set = None;
 				let mut block_finished = false;
-				match &mut prs.instruction.kind {
-					InstrKind::None => {
+				match &mut prs.mode {
+					ParseMode::Root => {
 						match tok {
 							Token::Routine => {
-								prs.instruction = Instruction::new_kind(InstrKind::Routine(String::new()));
+								if let Some(..) = prs.parsed.blocks.get_mut(&prs.block).expect("Block does not exist").parent {
+									return Err(PkgError::Parse(ParseError::UnexpectedRoutine(pos.clone())))
+								}
+								prs.mode = ParseMode::Routine(None);
+							}
+							Token::Ident(name) => {
+								match name.as_str() {
+									"if" => {
+										prs.mode = ParseMode::If(Vec::new());
+									},
+									name => {
+										prs.mode = ParseMode::Instruction(
+											Instruction::new(InstrKind::Unknown(name.to_owned(), Vec::new())),
+											Vec::new()
+										);
+									}
+								}
+							}
+							Token::Curly(side) => {
+								match side {
+									Side::Left => {
+										return Err(PkgError::Parse(ParseError::UnexpectedToken(tok.as_string(), pos.clone())))
+									}
+									Side::Right => {
+										block_finished = true;
+										prs.mode = ParseMode::Root;
+									}
+								}
+							}
+							_ => {}
+						}
+						Ok::<(), PkgError>(())
+					}
+					ParseMode::Routine(name) => {
+						if let Some(name) = name {
+							match tok {
+								Token::Curly(side) => {
+									match side {
+										Side::Left => {
+											prs.block = prs.parsed.new_routine(name);
+											prs.mode = ParseMode::Root;
+										}
+										Side::Right => {
+											return Err(PkgError::Parse(ParseError::UnexpectedToken(tok.as_string(), pos.clone())))
+										}
+									}
+								}
+								_ => {
+									return Err(PkgError::Parse(ParseError::UnexpectedToken(tok.as_string(), pos.clone())))
+								}
+							}
+						} else {
+							match tok {
+								Token::Ident(ident) => {
+									*name = Some(ident.to_string());
+								}
+								_ => {
+									return Err(PkgError::Parse(ParseError::UnexpectedToken(tok.as_string(), pos.clone())))
+								}
+							}
+						}
+						Ok(())
+					}
+					ParseMode::If(args) => {
+						match tok {
+							Token::Ident(name) => {
+								args.push(name.clone());
+							}
+							Token::Str(text) => {
+								args.push(text.clone());
+							}
+							Token::Curly(side) => {
+								match side {
+									Side::Left => {
+										prs.block = prs.parsed.new_block(Some(prs.block));
+										prs.new_instruction(Instruction::new(InstrKind::If(prs.block)));
+										prs.mode = ParseMode::Root;
+									}
+									Side::Right => {
+										return Err(PkgError::Parse(ParseError::UnexpectedToken(tok.as_string(), pos.clone())))
+									}
+								}
 							}
 							_ => {}
 						}
 						Ok(())
 					}
-					InstrKind::Routine(name) => {
+					ParseMode::Instruction(instr, args) => {
 						match tok {
-							Token::Ident(ident) => {
-								*name = ident.to_string();
-								instr_finished = true;
+							Token::Ident(name) => {
+								args.push(name.clone());
 							}
-							_ => {
-								return Err(PkgError::Parse(ParseError::UnexpectedToken(tok.as_string(), pos.clone())))
+							Token::Str(text) => {
+								args.push(text.clone());
 							}
+							Token::Semicolon => {
+								instr_to_push = Some(instr.clone());
+								mode_to_set = Some(ParseMode::Root);
+							}
+							_ => {}
+						}
+						match &mut instr.kind {
+							InstrKind::Unknown(.., instr_args) => {
+								*instr_args = args.clone();
+							}
+							_ => {}
 						}
 						Ok(())
 					}
-					_ => {
-						Ok::<(), PkgError>(())
-					}
 				}?;
 
-				if instr_finished {
-					prs.reset_instruction();
+				if let Some(instr) = instr_to_push {
+					prs.new_instruction(instr);
+				}
+
+				if let Some(mode) = mode_to_set {
+					prs.mode = mode;
+				}
+				
+				if block_finished {
+					prs.new_block();
 				}
 			}
 			dbg!(&prs);
