06ccb7cb2ac20325fbf9b5d17d003dfcb64c81d9
CarbonSmasher <crabonsmasher@gmail.com>
2023-04-19 18:53:07 -0400

    Small cleanup for Adoptium download

--- src/net/java.rs
+++ src/net/java.rs
@@ -17,11 +17,9 @@ pub mod adoptium {
 	/// Gets the newest Adoptium binaries download for a major Java version
 	pub async fn get_latest(major_version: &str) -> anyhow::Result<json::JsonObject> {
 		let url = json_url(major_version);
-		let manifest = download::json::<serde_json::Value>(&url)
+		let manifest = download::json::<Vec<serde_json::Value>>(&url)
 			.await
 			.context("Failed to download manifest of Adoptium versions")?;
-		let manifest = json::ensure_type(manifest.as_array(), JsonType::Arr)
-			.context("Expected manifest to be an array of versions")?;
 		let version = json::ensure_type(
 			manifest
 				.get(0)
