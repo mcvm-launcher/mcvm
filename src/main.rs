22c7758eb91dec0aa683c34936f30730a9105340
CarbonSmasher <crabonsmasher@gmail.com>
2023-09-13 07:56:57 -0400

    Fix double-printing of errors

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@ mod cli;
 use std::process::ExitCode;
 
 use cli::commands::run_cli;
-use color_print::cformat;
 
 #[tokio::main]
 async fn main() -> ExitCode {
 	let result = run_cli().await;
-	if let Err(e) = result {
-		eprintln!("{}", cformat!("<r>{:?}", e));
+	if result.is_err() {
 		return ExitCode::FAILURE;
 	}
 
