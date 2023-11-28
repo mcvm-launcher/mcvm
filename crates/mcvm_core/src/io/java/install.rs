ef2a9d4a824698c4417f5c3d0c67a87ff09a5dac
CarbonSmasher <crabonsmasher@gmail.com>
2023-11-27 21:56:20 -0500

    Add auto and system Java

--- crates/mcvm_core/src/io/java/install.rs
+++ crates/mcvm_core/src/io/java/install.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::BufReader;
 use std::path::{Path, PathBuf};
 
-use anyhow::Context;
+use anyhow::{bail, Context};
 use mcvm_shared::later::Later;
 use mcvm_shared::output::{MCVMOutput, MessageContents, MessageLevel};
 use reqwest::Client;
@@ -17,6 +17,11 @@ use mcvm_shared::util::preferred_archive_extension;
 /// Type of Java installation
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub enum JavaInstallationKind {
+	/// Automatically chooses different Java
+	/// flavors based on system conditions
+	Auto(Later<String>),
+	/// System Java
+	System(Later<String>),
 	/// Adoptium
 	Adoptium(Later<String>),
 	/// Azul Zulu
@@ -29,6 +34,7 @@ impl JavaInstallationKind {
 	/// Parse a string into a JavaKind
 	pub fn parse(string: &str) -> Self {
 		match string {
+			"system" => Self::System(Later::Empty),
 			"adoptium" => Self::Adoptium(Later::Empty),
 			"zulu" => Self::Zulu(Later::Empty),
 			path => Self::Custom(PathBuf::from(path)),
@@ -56,9 +62,10 @@ impl JavaInstallation {
 	/// Add a major version to a Java installation that supports it
 	pub fn add_version(&mut self, version: &str) {
 		match &mut self.kind {
-			JavaInstallationKind::Adoptium(vers) | JavaInstallationKind::Zulu(vers) => {
-				vers.fill(version.to_owned())
-			}
+			JavaInstallationKind::Auto(vers)
+			| JavaInstallationKind::System(vers)
+			| JavaInstallationKind::Adoptium(vers)
+			| JavaInstallationKind::Zulu(vers) => vers.fill(version.to_owned()),
 			JavaInstallationKind::Custom(..) => {}
 		};
 	}
@@ -81,41 +88,23 @@ impl JavaInstallation {
 		);
 
 		match &self.kind {
+			JavaInstallationKind::Auto(major_version) => {
+				self.path.fill(
+					install_auto(major_version.get(), paths, manager, lock, client, o).await?,
+				);
+			}
+			JavaInstallationKind::System(major_version) => {
+				self.path.fill(install_system(major_version.get())?);
+			}
 			JavaInstallationKind::Adoptium(major_version) => {
-				let directory = if manager.allow_offline {
-					if let Some(directory) =
-						lock.get_java_path(PersistentDataJavaInstallation::Adoptium, major_version.get())
-					{
-						Ok(directory)
-					} else {
-						update_adoptium(major_version.get(), lock, paths, client, o)
-							.await
-							.context("Failed to update Adoptium Java")
-					}
-				} else {
-					update_adoptium(major_version.get(), lock, paths, client, o)
-						.await
-						.context("Failed to update Adoptium Java")
-				}?;
-				self.path.fill(directory);
+				self.path.fill(
+					install_adoptium(major_version.get(), paths, manager, lock, client, o).await?,
+				);
 			}
 			JavaInstallationKind::Zulu(major_version) => {
-				let directory = if manager.allow_offline {
-					if let Some(directory) =
-						lock.get_java_path(PersistentDataJavaInstallation::Zulu, major_version.get())
-					{
-						Ok(directory)
-					} else {
-						update_zulu(major_version.get(), lock, paths, client, o)
-							.await
-							.context("Failed to update Zulu Java")
-					}
-				} else {
-					update_zulu(major_version.get(), lock, paths, client, o)
-						.await
-						.context("Failed to update Zulu Java")
-				}?;
-				self.path.fill(directory);
+				self.path.fill(
+					install_zulu(major_version.get(), paths, manager, lock, client, o).await?,
+				);
 			}
 			JavaInstallationKind::Custom(path) => {
 				self.path.fill(path.clone());
@@ -132,6 +121,88 @@ impl JavaInstallation {
 	/// Get the path to the JVM. Will panic if not installed.
 	pub fn get_jvm_path(&self) -> PathBuf {
 		self.path.get().join("bin/java")
+	}
+}
+
+async fn install_auto(
+	major_version: &str,
+	paths: &Paths,
+	manager: &UpdateManager,
+	lock: &mut PersistentData,
+	client: &Client,
+	o: &mut impl MCVMOutput,
+) -> anyhow::Result<PathBuf> {
+	let out = install_system(major_version);
+	if let Ok(out) = out {
+		return Ok(out);
+	}
+	let out = install_adoptium(major_version, paths, manager, lock, client, o).await;
+	if let Ok(out) = out {
+		return Ok(out);
+	}
+	let out = install_zulu(major_version, paths, manager, lock, client, o).await;
+	if let Ok(out) = out {
+		return Ok(out);
+	}
+	bail!("Failed to automatically install Java")
+}
+
+fn install_system(major_version: &str) -> anyhow::Result<PathBuf> {
+	let installation = get_system_java_installation(major_version);
+	if let Some(installation) = installation {
+		Ok(installation)
+	} else {
+		bail!("No valid system Java installation was found");
+	}
+}
+
+async fn install_adoptium(
+	major_version: &str,
+	paths: &Paths,
+	manager: &UpdateManager,
+	lock: &mut PersistentData,
+	client: &Client,
+	o: &mut impl MCVMOutput,
+) -> anyhow::Result<PathBuf> {
+	if manager.allow_offline {
+		if let Some(directory) =
+			lock.get_java_path(PersistentDataJavaInstallation::Adoptium, major_version)
+		{
+			Ok(directory)
+		} else {
+			update_adoptium(major_version, lock, paths, client, o)
+				.await
+				.context("Failed to update Adoptium Java")
+		}
+	} else {
+		update_adoptium(major_version, lock, paths, client, o)
+			.await
+			.context("Failed to update Adoptium Java")
+	}
+}
+
+async fn install_zulu(
+	major_version: &str,
+	paths: &Paths,
+	manager: &UpdateManager,
+	lock: &mut PersistentData,
+	client: &Client,
+	o: &mut impl MCVMOutput,
+) -> anyhow::Result<PathBuf> {
+	if manager.allow_offline {
+		if let Some(directory) =
+			lock.get_java_path(PersistentDataJavaInstallation::Zulu, major_version)
+		{
+			Ok(directory)
+		} else {
+			update_zulu(major_version, lock, paths, client, o)
+				.await
+				.context("Failed to update Zulu Java")
+		}
+	} else {
+		update_zulu(major_version, lock, paths, client, o)
+			.await
+			.context("Failed to update Zulu Java")
 	}
 }
 
@@ -284,4 +355,33 @@ fn extract_archive(arc_path: &Path, out_dir: &Path) -> anyhow::Result<()> {
 	}
 
 	Ok(())
+}
+
+/// Gets the optimal path to a system Java installation
+fn get_system_java_installation(#[allow(unused_variables)] major_version: &str) -> Option<PathBuf> {
+	#[cfg(target_os = "windows")]
+	{
+		// OpenJDK
+		let dir = PathBuf::from("C:/Program Files/Java");
+		if dir.exists() {
+			let read = std::fs::read_dir(dir);
+			if let Ok(read) = read {
+				for path in read {
+					let Ok(path) = path else { continue };
+					if !path.path().is_dir() {
+						continue;
+					}
+					let name = path.file_name().to_string_lossy().to_string();
+					if !name.starts_with("jdk-") {
+						continue;
+					}
+					if !name.contains(&format!("-{major_version}.")) {
+						continue;
+					}
+					return Some(path.path());
+				}
+			}
+		}
+	}
+	None
 }
