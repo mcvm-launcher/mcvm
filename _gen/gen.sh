737007a02fd02e803acea8602f65488569efcd06
CarbonSmasher <crabonsmasher@gmail.com>
2024-03-28 12:06:20 -0400

    Make package generation work in parallel

--- _gen/gen.sh
+++ _gen/gen.sh
@@ -1,24 +1,3 @@
 echo "Generating packages..."
-# Read packages file
-while read pkg; do
-	# Read properties from the line
-	LINE=($pkg)
-	PKG_NAME=${LINE[0]}
-	PKG_SOURCE=${LINE[1]}
-	PKG_ID=${LINE[2]}
-
-	# Print info
-  echo " - Generating package '$PKG_NAME'..."
-  echo "      From '$PKG_SOURCE' with ID '$PKG_ID'..."
-
-	# Create the generation config if it is not created already
-	PKG_GEN_CONFIG="./configs/$PKG_NAME.json"
-	if ! test -f $PKG_GEN_CONFIG; then
-		echo "{}" > $PKG_GEN_CONFIG;
-	fi
-
-	# Generate the package
-	OUTPUT=$(mcvm_tools gen-pkg $PKG_SOURCE $PKG_ID -c $PKG_GEN_CONFIG)
-	# Write the package file with JSON formatting
-	echo $OUTPUT | jq > "../std/api/mcvm/pkg/$PKG_NAME.json"
-done < packages.txt
+# Perform in parallel
+parallel -a packages.txt ./gen_one.sh
