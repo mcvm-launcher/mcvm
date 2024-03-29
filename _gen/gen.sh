eb4a5ef62f3dd027faa576a548cc956824e5b8ab
CarbonSmasher <crabonsmasher@gmail.com>
2024-03-28 21:14:34 -0400

    Regenerate packages with new batching

--- _gen/gen.sh
+++ _gen/gen.sh
@@ -1,5 +1,4 @@
 echo "Generating packages..."
 # Perform in parallel
-# We have to limit the number of jobs so that Modrinth doesn't complain about too many requests
 # parallel -a packages.txt --jobs 20 ./gen_one.sh
 mcvm_tools gen-pkg-batched config.json
