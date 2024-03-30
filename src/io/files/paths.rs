bb406744bd2d8316c2862d4efd794c5a21c8db93
CarbonSmasher <crabonsmasher@gmail.com>
2024-03-30 19:56:10 -0400

    Remove unused paths from paths struct

--- src/io/files/paths.rs
+++ src/io/files/paths.rs
@@ -12,24 +12,16 @@ pub struct Paths {
 	pub project: ProjectDirs,
 	/// Paths object from core
 	pub core: mcvm_core::Paths,
-	/// Holds data
+	/// Holds program data
 	pub data: PathBuf,
 	/// Holds internal data
 	pub internal: PathBuf,
-	/// Holds game assets
-	pub assets: PathBuf,
-	/// Holds game libraries
-	pub libraries: PathBuf,
-	/// Holds Java installations
-	pub java: PathBuf,
 	/// Holds addons
 	pub addons: PathBuf,
 	/// Holds cached package scripts
 	pub pkg_cache: PathBuf,
 	/// Holds cached package repository indexes
 	pub pkg_index_cache: PathBuf,
-	/// Holds game jar files
-	pub jars: PathBuf,
 	/// Holds log files
 	pub logs: PathBuf,
 	/// Holds launch log files
@@ -43,7 +35,7 @@ pub struct Paths {
 }
 
 impl Paths {
-	/// Create a new Paths object
+	/// Create a new Paths object and also create all of the paths it contains on the filesystem
 	pub async fn new() -> anyhow::Result<Paths> {
 		let base = BaseDirs::new().ok_or(anyhow!("Base directories failed"))?;
 		let project =
@@ -51,13 +43,9 @@ impl Paths {
 
 		let data = project.data_dir().to_owned();
 		let internal = data.join("internal");
-		let assets = internal.join("assets");
-		let libraries = internal.join("libraries");
-		let java = internal.join("java");
 		let addons = internal.join("addons");
 		let pkg_cache = project.cache_dir().join("pkg");
 		let pkg_index_cache = pkg_cache.join("index");
-		let jars = internal.join("jars");
 		let logs = data.join("logs");
 		let launch_logs = logs.join("launch");
 		let run = project
@@ -71,31 +59,26 @@ impl Paths {
 		tokio::fs::create_dir_all(project.cache_dir()).await?;
 		tokio::fs::create_dir_all(project.config_dir()).await?;
 		tokio::fs::create_dir_all(&internal).await?;
-		tokio::fs::create_dir_all(&assets).await?;
-		tokio::fs::create_dir_all(&java).await?;
 		tokio::fs::create_dir_all(&addons).await?;
 		tokio::fs::create_dir_all(&pkg_cache).await?;
 		tokio::fs::create_dir_all(&pkg_index_cache).await?;
-		tokio::fs::create_dir_all(&jars).await?;
 		tokio::fs::create_dir_all(&logs).await?;
 		tokio::fs::create_dir_all(&launch_logs).await?;
 		tokio::fs::create_dir_all(&run).await?;
 		tokio::fs::create_dir_all(&snapshots).await?;
 		tokio::fs::create_dir_all(&fabric_quilt).await?;
 
+		let core_paths = mcvm_core::Paths::new().context("Failed to create core paths")?;
+
 		Ok(Paths {
 			base,
 			project,
-			core: mcvm_core::Paths::new().context("Failed to create core paths")?,
+			core: core_paths,
 			data,
 			internal,
-			assets,
-			libraries,
-			java,
 			addons,
 			pkg_cache,
 			pkg_index_cache,
-			jars,
 			logs,
 			launch_logs,
 			run,
