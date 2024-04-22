43ed6bbdf17a484e45adcfcefeb163e813876ec0
CarbonSmasher <crabonsmasher@gmail.com>
2024-04-22 14:45:15 -0400

    Refactor system Java scanning

--- crates/core/src/io/java/install/system.rs
+++ crates/core/src/io/java/install/system.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use anyhow::bail;
 
@@ -16,51 +16,63 @@ pub fn install(major_version: &str) -> anyhow::Result<PathBuf> {
 fn get_system_java_installation(#[allow(unused_variables)] major_version: &str) -> Option<PathBuf> {
 	#[cfg(target_os = "windows")]
 	{
-		// OpenJDK
-		let dir = PathBuf::from("C:/Program Files/Java");
-		if dir.exists() {
-			let read = std::fs::read_dir(dir);
-			if let Ok(read) = read {
-				for path in read {
-					let Ok(path) = path else { continue };
-					if !path.path().is_dir() {
-						continue;
-					}
-					let name = path.file_name().to_string_lossy().to_string();
-					if !name.starts_with("jdk-") {
-						continue;
-					}
-					if !name.contains(&format!("-{major_version}.")) {
-						continue;
-					}
-					return Some(path.path());
-				}
-			}
+		if let Some(path) = scan_windows(major_version) {
+			return Some(path);
 		}
 	}
 	#[cfg(target_os = "linux")]
 	{
-		// OpenJDK
-		let dir = PathBuf::from("/usr/lib/jvm");
-		if dir.exists() {
-			let read = std::fs::read_dir(dir);
-			if let Ok(read) = read {
-				for path in read {
-					let Ok(path) = path else { continue };
-					if !path.path().is_dir() {
-						continue;
-					}
-					let name = path.file_name().to_string_lossy().to_string();
-					if !name.starts_with("java-") {
-						continue;
-					}
-					if !name.contains(&format!("-{major_version}-")) {
-						continue;
-					}
-					return Some(path.path());
-				}
-			}
+		if let Some(path) = scan_linux(major_version) {
+			return Some(path);
 		}
 	}
+
+	None
+}
+
+/// Scan for Java on Windows
+#[cfg(target_os = "windows")]
+fn scan_windows(major_version: &str) -> Option<PathBuf> {
+	// OpenJDK
+	if let Some(path) = scan_dir(&PathBuf::from("C:/Program Files/Java"), major_version) {
+		return Some(path);
+	}
+
+	None
+}
+
+/// Scan for Java on Linux
+#[cfg(target_os = "linux")]
+fn scan_linux(major_version: &str) -> Option<PathBuf> {
+	// OpenJDK
+	if let Some(path) = scan_dir(&PathBuf::from("/usr/lib/jvm"), major_version) {
+		return Some(path);
+	}
+
+	None
+}
+
+/// Scan a directory for Java installations
+fn scan_dir(dir: &Path, major_version: &str) -> Option<PathBuf> {
+	if dir.exists() {
+		let read = std::fs::read_dir(dir).ok()?;
+		for path in read {
+			let Ok(path) = path else { continue };
+			if !path.path().is_dir() {
+				continue;
+			}
+			let name = path.file_name().to_string_lossy().to_string();
+			if !(name.starts_with("java-") || name.starts_with("jdk-")) {
+				continue;
+			}
+			if !(name.contains(&format!("-{major_version}-"))
+				|| name.contains(&format!("-{major_version}.")))
+			{
+				continue;
+			}
+			return Some(path.path());
+		}
+	}
+
 	None
 }
