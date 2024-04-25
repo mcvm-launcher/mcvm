3b3e51092332ce7f7f6ffadc35d5c0a57a7be5de
CarbonSmasher <crabonsmasher@gmail.com>
2024-04-25 16:50:33 -0400

    Rework how snapshots are handled

--- plugins/plugins/snapshot/main.rs
+++ plugins/plugins/snapshot/main.rs
@@ -1,19 +1,15 @@
 mod snapshot;
 
-use std::{
-	collections::HashMap,
-	path::{Path, PathBuf},
-};
+use std::collections::HashMap;
+use std::path::PathBuf;
 
 use anyhow::Context;
 use clap::Parser;
 use color_print::cprintln;
-use mcvm_plugin::{
-	api::{CustomPlugin, HookContext},
-	hooks,
-};
+use mcvm_plugin::api::{CustomPlugin, HookContext};
+use mcvm_plugin::hooks;
 use mcvm_shared::id::InstanceRef;
-use snapshot::{Config, Index};
+use snapshot::{get_snapshot_directory, Config, Index, DEFAULT_GROUP};
 
 use crate::snapshot::SnapshotKind;
 
@@ -30,11 +26,27 @@ fn main() -> anyhow::Result<()> {
 		let it = std::iter::once(format!("mcvm {subcommand}")).chain(args.into_iter().skip(1));
 		let cli = Cli::parse_from(it);
 		let result = match cli.command {
-			Subcommand::List { raw, instance } => list(&ctx, raw, &instance),
-			Subcommand::Create { instance, snapshot } => create(&ctx, &instance, &snapshot),
-			Subcommand::Remove { instance, snapshot } => remove(&ctx, &instance, &snapshot),
-			Subcommand::Restore { instance, snapshot } => restore(&ctx, &instance, &snapshot),
-			Subcommand::Info { instance, snapshot } => info(&ctx, &instance, &snapshot),
+			Subcommand::List {
+				raw,
+				instance,
+				group,
+			} => list(&ctx, raw, &instance, group.as_deref()),
+			Subcommand::Create { instance, group } => create(&ctx, &instance, group.as_deref()),
+			Subcommand::Remove {
+				instance,
+				group,
+				snapshot,
+			} => remove(&ctx, &instance, group.as_deref(), &snapshot),
+			Subcommand::Restore {
+				instance,
+				group,
+				snapshot,
+			} => restore(&ctx, &instance, group.as_deref(), &snapshot),
+			Subcommand::Info {
+				instance,
+				group,
+				snapshot,
+			} => info(&ctx, &instance, group.as_deref(), &snapshot),
 		};
 		result?;
 
@@ -61,18 +73,22 @@ enum Subcommand {
 		raw: bool,
 		/// The instance to list snapshots for
 		instance: String,
+		/// The group to list snapshots for
+		group: Option<String>,
 	},
 	#[command(about = "Create a snapshot")]
 	Create {
 		/// The instance to create a snapshot for
 		instance: String,
-		/// The id of the snapshot
-		snapshot: String,
+		/// The group to create the snapshot for
+		group: Option<String>,
 	},
 	#[command(about = "Remove an existing snapshot")]
 	Remove {
 		/// The instance the snapshot is in
 		instance: String,
+		/// The group the snapshot is in
+		group: Option<String>,
 		/// The snapshot to remove
 		snapshot: String,
 	},
@@ -80,6 +96,8 @@ enum Subcommand {
 	Restore {
 		/// The instance the snapshot is in
 		instance: String,
+		/// The group the snapshot is in
+		group: Option<String>,
 		/// The snapshot to restore
 		snapshot: String,
 	},
@@ -87,17 +105,31 @@ enum Subcommand {
 	Info {
 		/// The instance the snapshot is in
 		instance: String,
+		/// The group the snapshot is in
+		group: Option<String>,
 		/// The snapshot to get info about
 		snapshot: String,
 	},
 }
 
-fn list(ctx: &HookContext<'_, hooks::Subcommand>, raw: bool, instance: &str) -> anyhow::Result<()> {
-	let snapshots_dir = get_snapshots_dir(ctx)?;
-	let instance_snap_dir = get_instance_snapshot_dir(instance, &snapshots_dir)?;
-	let index = Index::open(&instance_snap_dir)?;
+fn list(
+	ctx: &HookContext<'_, hooks::Subcommand>,
+	raw: bool,
+	instance: &str,
+	group: Option<&str>,
+) -> anyhow::Result<()> {
+	let inst_ref =
+		InstanceRef::parse(instance.into()).context("Failed to parse instance reference")?;
+	let group = group.unwrap_or(DEFAULT_GROUP);
 
-	for snapshot in &index.snapshots {
+	let index = get_index(ctx, &inst_ref)?;
+	let group = index
+		.contents
+		.groups
+		.get(group)
+		.context("Group does not exist")?;
+
+	for snapshot in &group.snapshots {
 		if raw {
 			println!("{}", snapshot.id);
 		} else {
@@ -105,44 +137,30 @@ fn list(ctx: &HookContext<'_, hooks::Subcommand>, raw: bool, instance: &str) -> 
 		}
 	}
 
-	index.finish(&instance_snap_dir)?;
+	index.finish()?;
 	Ok(())
 }
 
 fn create(
 	ctx: &HookContext<'_, hooks::Subcommand>,
 	instance: &str,
-	snapshot: &str,
+	group: Option<&str>,
 ) -> anyhow::Result<()> {
 	let inst_ref =
 		InstanceRef::parse(instance.into()).context("Failed to parse instance reference")?;
+	let group = group.unwrap_or(DEFAULT_GROUP);
 
-	let snapshots_dir = get_snapshots_dir(ctx)?;
-	let instance_snap_dir = get_instance_snapshot_dir(&inst_ref.instance, &snapshots_dir)?;
-	let mut index = Index::open(&instance_snap_dir)?;
+	let mut index = get_index(ctx, &inst_ref)?;
 
-	if index.snapshot_exists(snapshot) {
-		cprintln!("<y>Warning: Overwriting existing snapshot with same ID");
-	}
-
-	let config = get_snapshot_config(&inst_ref, ctx)
-		.context("Failed to get snapshot config for instance")?;
 	let inst_dir = ctx
 		.get_data_dir()?
 		.join("instances")
 		.join(inst_ref.profile.to_string())
 		.join(&inst_ref.instance.to_string());
 
-	index.create_snapshot(
-		SnapshotKind::User,
-		snapshot.into(),
-		&config,
-		&inst_ref.instance,
-		&inst_dir,
-		&instance_snap_dir,
-	)?;
+	index.create_snapshot(SnapshotKind::User, Some(group), &inst_dir)?;
 
-	index.finish(&instance_snap_dir)?;
+	index.finish()?;
 
 	cprintln!("<g>Snapshot created.");
 
@@ -152,17 +170,17 @@ fn create(
 fn remove(
 	ctx: &HookContext<'_, hooks::Subcommand>,
 	instance: &str,
+	group: Option<&str>,
 	snapshot: &str,
 ) -> anyhow::Result<()> {
 	let inst_ref =
 		InstanceRef::parse(instance.into()).context("Failed to parse instance reference")?;
+	let group = group.unwrap_or(DEFAULT_GROUP);
 
-	let snapshots_dir = get_snapshots_dir(ctx)?;
-	let instance_snap_dir = get_instance_snapshot_dir(&inst_ref.instance, &snapshots_dir)?;
-	let mut index = Index::open(&instance_snap_dir)?;
+	let mut index = get_index(ctx, &inst_ref)?;
 
-	index.remove_snapshot(snapshot, &inst_ref.instance, &instance_snap_dir)?;
-	index.finish(&instance_snap_dir)?;
+	index.remove_snapshot(group, snapshot)?;
+	index.finish()?;
 
 	cprintln!("<g>Snapshot removed.");
 
@@ -172,14 +190,14 @@ fn remove(
 fn restore(
 	ctx: &HookContext<'_, hooks::Subcommand>,
 	instance: &str,
+	group: Option<&str>,
 	snapshot: &str,
 ) -> anyhow::Result<()> {
 	let inst_ref =
 		InstanceRef::parse(instance.into()).context("Failed to parse instance reference")?;
+	let group = group.unwrap_or(DEFAULT_GROUP);
 
-	let snapshots_dir = get_snapshots_dir(ctx)?;
-	let instance_snap_dir = get_instance_snapshot_dir(&inst_ref.instance, &snapshots_dir)?;
-	let index = Index::open(&instance_snap_dir)?;
+	let index = get_index(ctx, &inst_ref)?;
 
 	let inst_dir = ctx
 		.get_data_dir()?
@@ -187,8 +205,8 @@ fn restore(
 		.join(inst_ref.profile.to_string())
 		.join(&inst_ref.instance.to_string());
 
-	index.restore_snapshot(snapshot, &inst_ref.instance, &inst_dir, &instance_snap_dir)?;
-	index.finish(&instance_snap_dir)?;
+	index.restore_snapshot(group, snapshot, &inst_dir)?;
+	index.finish()?;
 
 	cprintln!("<g>Snapshot restored.");
 
@@ -198,20 +216,16 @@ fn restore(
 fn info(
 	ctx: &HookContext<'_, hooks::Subcommand>,
 	instance: &str,
+	group: Option<&str>,
 	snapshot_id: &str,
 ) -> anyhow::Result<()> {
 	let inst_ref =
 		InstanceRef::parse(instance.into()).context("Failed to parse instance reference")?;
+	let group = group.unwrap_or(DEFAULT_GROUP);
 
-	let snapshots_dir = get_snapshots_dir(ctx)?;
-	let instance_snap_dir = get_instance_snapshot_dir(&inst_ref.instance, &snapshots_dir)?;
-	let index = Index::open(&instance_snap_dir)?;
+	let index = get_index(ctx, &inst_ref)?;
 
-	let snapshot = index
-		.snapshots
-		.iter()
-		.find(|x| x.id == snapshot_id)
-		.context("Snapshot does not exist")?;
+	let snapshot = index.get_snapshot(group, snapshot_id)?;
 
 	cprintln!(
 		"<s>Snapshot <b>{}</b> in instance <g>{}</g>:",
@@ -223,14 +237,16 @@ fn info(
 	Ok(())
 }
 
-fn get_snapshots_dir(ctx: &HookContext<'_, hooks::Subcommand>) -> anyhow::Result<PathBuf> {
-	let dir = ctx.get_data_dir()?.join("snapshots");
-	std::fs::create_dir_all(&dir)?;
-	Ok(dir)
+fn get_index(
+	ctx: &HookContext<'_, hooks::Subcommand>,
+	inst_ref: &InstanceRef,
+) -> anyhow::Result<Index> {
+	let dir = get_snapshot_directory(&get_snapshots_dir(ctx)?, inst_ref);
+	Index::open(&dir, inst_ref.clone(), &get_snapshot_config(inst_ref, ctx)?)
 }
 
-fn get_instance_snapshot_dir(instance: &str, snapshots_dir: &Path) -> anyhow::Result<PathBuf> {
-	let dir = snapshots_dir.join(instance);
+fn get_snapshots_dir(ctx: &HookContext<'_, hooks::Subcommand>) -> anyhow::Result<PathBuf> {
+	let dir = ctx.get_data_dir()?.join("snapshots");
 	std::fs::create_dir_all(&dir)?;
 	Ok(dir)
 }
--- plugins/plugins/snapshot/snapshot.rs
+++ plugins/plugins/snapshot/snapshot.rs
@@ -1,8 +1,11 @@
+use std::collections::HashMap;
 use std::fs::{self, File};
 use std::io::{BufReader, BufWriter, Read};
 use std::path::{Path, PathBuf};
 
-use anyhow::{anyhow, Context};
+use anyhow::{anyhow, bail, Context};
+use mcvm_core::io::json_to_file;
+use mcvm_shared::id::InstanceRef;
 use mcvm_shared::util::utc_timestamp;
 use rand::Rng;
 #[cfg(feature = "schema")]
@@ -12,13 +15,60 @@ use zip::{ZipArchive, ZipWriter};
 
 /// Name of the snapshot index file
 pub const INDEX_NAME: &str = "index.json";
+/// ID of the default group
+pub const DEFAULT_GROUP: &str = "default";
 
 /// Settings for snapshots
 #[derive(Serialize, Deserialize, Default, Clone, Debug)]
 #[cfg_attr(feature = "schema", derive(JsonSchema))]
 #[serde(default)]
 pub struct Config {
-	/// The max number of snapshots for an instance
+	/// Default settings for snapshots
+	#[serde(flatten)]
+	pub common: CommonConfig,
+	/// Snapshot groups
+	pub groups: HashMap<String, GroupConfig>,
+}
+
+impl Config {
+	/// Get the consolidated, final config for a group
+	pub fn get_group_config(&self, group_id: &str) -> anyhow::Result<GroupConfig> {
+		let mut out = GroupConfig {
+			common: self.common.clone(),
+			..Default::default()
+		};
+
+		if group_id == DEFAULT_GROUP {
+			return Ok(out);
+		};
+
+		let Some(group) = self.groups.get(group_id) else {
+			bail!("Group does not exist");
+		};
+		let group = group.clone();
+
+		out.on = out.on.or(group.on);
+
+		Ok(out)
+	}
+}
+
+/// Configuration for a group of snapshots
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
+#[serde(default)]
+pub struct GroupConfig {
+	/// When the snapshot should be automatically created
+	pub on: Option<SnapshotAutoHook>,
+	/// Snapshot settings for this group
+	#[serde(flatten)]
+	pub common: CommonConfig,
+}
+
+/// General configuration for snapshots and snapshot groups
+#[derive(Serialize, Deserialize, Default, Clone, Debug)]
+#[serde(default)]
+pub struct CommonConfig {
+	/// The max number of snapshots
 	pub max_count: Option<u32>,
 	/// The files and directories to include in the snapshot
 	pub paths: Vec<String>,
@@ -26,12 +76,28 @@ pub struct Config {
 	pub storage_type: StorageType,
 }
 
+/// When a snapshot should be automatically created
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub enum SnapshotAutoHook {}
+
 /// Index for the snapshots of an instance
+pub struct Index {
+	/// The contents of the index
+	pub contents: IndexContents,
+	/// The path where the snapshots are
+	pub dir: PathBuf,
+	/// The config for the snapshots
+	pub config: Config,
+	/// The instance ref for this index
+	pub inst_ref: InstanceRef,
+}
+
+/// Contents for the snapshot index
 #[derive(Serialize, Deserialize, Default)]
 #[serde(default)]
-pub struct Index {
-	/// The list of available snapshots
-	pub snapshots: Vec<Entry>,
+pub struct IndexContents {
+	/// The list of available groups
+	pub groups: HashMap<String, GroupEntry>,
 }
 
 impl Index {
@@ -41,55 +107,71 @@ impl Index {
 	}
 
 	/// Open the index
-	pub fn open(snapshot_directory: &Path) -> anyhow::Result<Self> {
+	pub fn open(
+		snapshot_directory: &Path,
+		inst_ref: InstanceRef,
+		config: &Config,
+	) -> anyhow::Result<Self> {
 		fs::create_dir_all(snapshot_directory)?;
 		let path = Self::get_path(snapshot_directory);
 		let contents = if path.exists() {
 			let mut file = File::open(&path).context("Failed to open snapshot index")?;
 			serde_json::from_reader(&mut file).context("Failed to parse JSON")?
 		} else {
-			Self::default()
+			IndexContents::default()
+		};
+		let index = Self {
+			contents,
+			dir: snapshot_directory.to_owned(),
+			inst_ref,
+			config: config.clone(),
 		};
 
-		Ok(contents)
+		Ok(index)
 	}
 
 	/// Finish using the index
-	pub fn finish(&self, snapshot_directory: &Path) -> anyhow::Result<()> {
-		let path = Self::get_path(snapshot_directory);
-		let mut file = File::create(path)?;
-		serde_json::to_writer_pretty(&mut file, self)?;
+	pub fn finish(&self) -> anyhow::Result<()> {
+		let path = Self::get_path(&self.dir);
+		json_to_file(path, &self.contents)?;
 
 		Ok(())
 	}
 
-	/// Checks if a snapshot with an ID exists already
-	pub fn snapshot_exists(&self, snapshot_id: &str) -> bool {
-		self.snapshots.iter().any(|x| x.id == snapshot_id)
+	/// Get a snapshot
+	pub fn get_snapshot(&self, group_id: &str, snapshot_id: &str) -> anyhow::Result<&Entry> {
+		self.contents
+			.groups
+			.get(group_id)
+			.context("Group does not exist")?
+			.snapshots
+			.iter()
+			.find(|x| x.id == snapshot_id)
+			.context("Snapshot does not exist")
 	}
 
 	/// Create a new snapshot
 	pub fn create_snapshot(
 		&mut self,
 		kind: SnapshotKind,
-		snapshot_id: String,
-		config: &Config,
-		instance_id: &str,
+		group_id: Option<&str>,
 		instance_dir: &Path,
-		snapshots_dir: &Path,
 	) -> anyhow::Result<()> {
-		// Remove the snapshot if it exists already
-		if self.snapshot_exists(&snapshot_id) {
-			self.remove_snapshot(&snapshot_id, instance_id, snapshots_dir)
-				.context("Failed to remove existing snapshot with same ID")?;
-		}
+		let group_id = group_id.unwrap_or(DEFAULT_GROUP);
+		// // Remove the snapshot if it exists already
+		// if self.snapshot_exists(&snapshot_id) {
+		// 	self.remove_snapshot(&snapshot_id)
+		// 		.context("Failed to remove existing snapshot with same ID")?;
+		// }
 
-		let snapshot_dir = get_snapshot_directory(instance_id, snapshots_dir);
+		let group_config = self.config.get_group_config(group_id)?;
 
-		let snapshot_path = get_snapshot_path(&snapshot_dir, &snapshot_id, config.storage_type);
+		let snapshot_id = generate_random_id();
+		let snapshot_path =
+			self.get_snapshot_path(group_id, &snapshot_id, group_config.common.storage_type);
 
 		let mut readers = Vec::new();
-		for path in &config.paths {
+		for path in &group_config.common.paths {
 			let paths = get_instance_file_paths(path, instance_dir)
 				.context("Failed to get recursive file paths")?;
 			for path in paths {
@@ -99,45 +181,51 @@ impl Index {
 				readers.push((path.clone(), file));
 			}
 		}
-		write_snapshot_files(&snapshot_path, config, readers)?;
+		write_snapshot_files(&snapshot_path, &group_config, readers)?;
 
 		let now = utc_timestamp()?;
-		self.snapshots.push(Entry {
+		// Add the snapshot entry to the group
+		let group_entry = self
+			.contents
+			.groups
+			.entry(group_id.into())
+			.or_insert_with(GroupEntry::default);
+		group_entry.snapshots.push(Entry {
 			id: snapshot_id,
 			date: now,
 			kind,
-			storage_type: config.storage_type,
+			storage_type: group_config.common.storage_type,
 		});
 
-		self.remove_old_snapshots(config, instance_id, snapshots_dir)?;
+		self.remove_old_snapshots(group_id, &group_config)?;
 
 		Ok(())
 	}
 
 	/// Remove a snapshot
-	pub fn remove_snapshot(
-		&mut self,
-		snapshot_id: &str,
-		instance_id: &str,
-		snapshot_dir: &Path,
-	) -> anyhow::Result<()> {
-		let index = self
+	pub fn remove_snapshot(&mut self, group_id: &str, snapshot_id: &str) -> anyhow::Result<()> {
+		let group_entry = self
+			.contents
+			.groups
+			.entry(group_id.into())
+			.or_insert_with(GroupEntry::default);
+		let index = group_entry
 			.snapshots
 			.iter()
 			.position(|x| x.id == snapshot_id)
 			.ok_or(anyhow!("Snapshot with ID was not found"))?;
-		let snapshot = &mut self.snapshots[index];
+		let snapshot = &group_entry.snapshots[index];
+		let storage_type = snapshot.storage_type;
 
-		let snapshot_dir = get_snapshot_directory(instance_id, snapshot_dir);
-		let snapshot_path = get_snapshot_path(&snapshot_dir, snapshot_id, snapshot.storage_type);
+		group_entry.snapshots.remove(index);
+
+		let snapshot_path = self.get_snapshot_path(group_id, snapshot_id, storage_type);
 		if snapshot_path.exists() {
-			match snapshot.storage_type {
+			match storage_type {
 				StorageType::Archive => fs::remove_file(snapshot_path)?,
 				StorageType::Folder => fs::remove_dir_all(snapshot_path)?,
 			}
 		}
-
-		self.snapshots.remove(index);
 
 		Ok(())
 	}
@@ -145,19 +233,21 @@ impl Index {
 	/// Remove old snapshots that are over the limit
 	pub fn remove_old_snapshots(
 		&mut self,
-		config: &Config,
-		instance_id: &str,
-		snapshot_dir: &Path,
+		group_id: &str,
+		group_config: &GroupConfig,
 	) -> anyhow::Result<()> {
-		if let Some(limit) = config.max_count {
-			if self.snapshots.len() > (limit as usize) {
-				let num_to_remove = self.snapshots.len() - (limit as usize);
-				let to_remove: Vec<String> = self.snapshots[0..num_to_remove - 1]
+		let Some(group_entry) = self.contents.groups.get(group_id) else {
+			return Ok(());
+		};
+		if let Some(limit) = group_config.common.max_count {
+			if group_entry.snapshots.len() > (limit as usize) {
+				let num_to_remove = group_entry.snapshots.len() - (limit as usize);
+				let to_remove: Vec<String> = group_entry.snapshots[0..num_to_remove - 1]
 					.iter()
 					.map(|x| x.id.clone())
 					.collect();
 				for id in to_remove {
-					self.remove_snapshot(&id, instance_id, snapshot_dir)
+					self.remove_snapshot(group_id, &id)
 						.with_context(|| format!("Failed to remove old snapshot '{id}'"))?;
 				}
 			}
@@ -169,23 +259,55 @@ impl Index {
 	/// Restores a snapshot
 	pub fn restore_snapshot(
 		&self,
+		group_id: &str,
 		snapshot_id: &str,
-		instance_id: &str,
 		instance_dir: &Path,
-		snapshot_dir: &Path,
 	) -> anyhow::Result<()> {
-		let snapshot = self
+		let group_entry = self
+			.contents
+			.groups
+			.get(group_id)
+			.context("Group does not exist")?;
+		let snapshot = group_entry
 			.snapshots
 			.iter()
 			.find(|x| x.id == snapshot_id)
 			.ok_or(anyhow!("Snapshot with ID was not found"))?;
 
-		let snapshot_dir = get_snapshot_directory(instance_id, snapshot_dir);
-		let snapshot_path = get_snapshot_path(&snapshot_dir, snapshot_id, snapshot.storage_type);
+		let snapshot_path = self.get_snapshot_path(group_id, snapshot_id, snapshot.storage_type);
 		restore_snapshot_files(&snapshot_path, snapshot.storage_type, instance_dir)?;
 
 		Ok(())
 	}
+
+	/// Gets the snapshot directory for a group
+	fn get_group_dir(&self, group_id: &str) -> PathBuf {
+		self.dir.join(group_id)
+	}
+
+	/// Get the path to a specific snapshot
+	pub fn get_snapshot_path(
+		&self,
+		group_id: &str,
+		snapshot_id: &str,
+		storage_type: StorageType,
+	) -> PathBuf {
+		let path = self.get_group_dir(group_id);
+		let filename = match storage_type {
+			StorageType::Archive => format!("{snapshot_id}.zip"),
+			StorageType::Folder => snapshot_id.to_owned(),
+		};
+
+		path.join(filename)
+	}
+}
+
+/// A group entry in the snapshot index
+#[derive(Serialize, Deserialize, Default)]
+#[serde(default)]
+pub struct GroupEntry {
+	/// The snapshots in this group
+	pub snapshots: Vec<Entry>,
 }
 
 /// Entry for a snapshot in the snapshot index
@@ -222,22 +344,10 @@ pub enum StorageType {
 }
 
 /// Get the snapshot directory for an instance
-pub fn get_snapshot_directory(instance_id: &str, base_dir: &Path) -> PathBuf {
-	base_dir.join(instance_id)
-}
-
-/// Get the path to a specific snapshot
-pub fn get_snapshot_path(
-	snapshot_directory: &Path,
-	snapshot_id: &str,
-	storage_type: StorageType,
-) -> PathBuf {
-	let filename = match storage_type {
-		StorageType::Archive => format!("{snapshot_id}.zip"),
-		StorageType::Folder => snapshot_id.to_owned(),
-	};
-
-	snapshot_directory.join(filename)
+pub fn get_snapshot_directory(base_dir: &Path, inst_ref: &InstanceRef) -> PathBuf {
+	base_dir
+		.join(inst_ref.profile.to_string())
+		.join(inst_ref.instance.to_string())
 }
 
 /// Generates a random snapshot ID
@@ -276,10 +386,10 @@ fn get_instance_file_paths(path: &str, instance_dir: &Path) -> anyhow::Result<Ve
 /// Readers are pairs of relative file paths and readers for files.
 fn write_snapshot_files<R: Read>(
 	snapshot_path: &Path,
-	config: &Config,
+	group_config: &GroupConfig,
 	readers: Vec<(String, R)>,
 ) -> anyhow::Result<()> {
-	match &config.storage_type {
+	match &group_config.common.storage_type {
 		StorageType::Archive => {
 			let file = File::create(snapshot_path)?;
 			let mut file = BufWriter::new(file);
