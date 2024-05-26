a19b9ae1533f2e7ddfaac49fabe079c982dfca30
CarbonSmasher <crabonsmasher@gmail.com>
2024-05-26 16:18:33 -0400

    fix(pkg): Update std repo URL again

--- src/pkg/repo.rs
+++ src/pkg/repo.rs
@@ -72,7 +72,7 @@ impl PkgRepo {
 	pub fn std() -> Self {
 		Self::new(
 			"std",
-			PkgRepoLocation::Remote("https://mcvmlauncher.github.io/mcvm/std".into()),
+			PkgRepoLocation::Remote("https://mcvm-launcher.github.io/packages/std".into()),
 		)
 	}
 
