dce67d6b14c66a8d79f117b40c5340709dde9571
CarbonSmasher <crabonsmasher@gmail.com>
2025-01-01 18:19:16 -0500

    fix: This behaviors actually better

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -2,9 +2,16 @@ mod commands;
 mod output;
 mod secrets;
 
+use std::process::ExitCode;
+
 use commands::run_cli;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
-	run_cli().await
+async fn main() -> ExitCode {
+	let result = run_cli().await;
+	if result.is_err() {
+		return ExitCode::FAILURE;
+	}
+
+	ExitCode::SUCCESS
 }
