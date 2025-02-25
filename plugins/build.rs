53ee4bc4d9467f06fe7d5fc96986af5caa5b6205
CarbonSmasher <crabonsmasher@gmail.com>
2025-02-25 10:14:07 -0500

    fix(docs): Change docs location

--- plugins/build.rs
+++ plugins/build.rs
@@ -4,7 +4,7 @@ use zip::{write::FileOptions, CompressionMethod, ZipWriter};
 
 macro_rules! add_file {
 	($zip:expr, $path:literal) => {
-		let path = concat!("../docs/content/docs/", $path);
+		let path = concat!("../docs/docs/", $path);
 		$zip.start_file(
 			$path,
 			FileOptions::<()>::default().compression_method(CompressionMethod::Deflated),
@@ -19,27 +19,28 @@ fn main() {
 	let out = File::create("./zipped_docs.zip").unwrap();
 	let mut zip = ZipWriter::new(out);
 
-	add_file!(zip, "_index.md");
+	add_file!(zip, "index.md");
 	add_file!(zip, "configuring.md");
 	add_file!(zip, "modifications.md");
 	add_file!(zip, "principles.md");
 	zip.add_directory("packages", FileOptions::<()>::default())
 		.unwrap();
 	add_file!(zip, "packages/declarative.md");
-	add_file!(zip, "packages/_index.md");
+	add_file!(zip, "packages/index.md");
 	add_file!(zip, "packages/scripts.md");
 	zip.add_directory("guide", FileOptions::<()>::default())
 		.unwrap();
-	add_file!(zip, "guide/_index.md");
+	add_file!(zip, "guide/index.md");
 	zip.add_directory("plugins", FileOptions::<()>::default())
 		.unwrap();
-	add_file!(zip, "plugins/_index.md");
+	add_file!(zip, "plugins/index.md");
 	add_file!(zip, "plugins/user_guide.md");
 	zip.add_directory("plugins/development", FileOptions::<()>::default())
 		.unwrap();
-	add_file!(zip, "plugins/development/_index.md");
+	add_file!(zip, "plugins/development/index.md");
 	add_file!(zip, "plugins/development/format.md");
-	add_file!(zip, "plugins/plugins/_index.md");
+	add_file!(zip, "plugins/development/hooks.md");
+	add_file!(zip, "plugins/plugins/index.md");
 	add_file!(zip, "plugins/plugins/args.md");
 	add_file!(zip, "plugins/plugins/backup.md");
 	add_file!(zip, "plugins/plugins/docs.md");
