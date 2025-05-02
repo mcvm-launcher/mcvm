4973eb4ddd68efa2c5dfff75b2f4b7ea5df1d84b
CarbonSmasher <crabonsmasher@gmail.com>
2025-05-02 13:59:14 -0400

    fix: Bruh

--- plugins/build.rs
+++ plugins/build.rs
@@ -46,7 +46,6 @@ fn main() {
 	add_file!(zip, "plugins/plugins/backup.md");
 	add_file!(zip, "plugins/plugins/custom_files.md");
 	add_file!(zip, "plugins/plugins/docs.md");
-	add_file!(zip, "plugins/plugins/custom_files.md");
 	add_file!(zip, "plugins/plugins/extra_versions.md");
 	add_file!(zip, "plugins/plugins/gen_pkg.md");
 	add_file!(zip, "plugins/plugins/lang.md");
