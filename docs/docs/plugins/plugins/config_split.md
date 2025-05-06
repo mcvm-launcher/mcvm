bec024eaa072abc972f6184d51449f75f7e14098
CarbonSmasher <crabonsmasher@gmail.com>
2025-05-06 10:03:44 -0400

    feat(plugins): Add config_split plugin

---
+++ docs/docs/plugins/plugins/config_split.md
@@ -0,0 +1,7 @@
+# Config Split
+ID: `config_split`
+
+The Config Split plugin allows you to have some parts of your configuration in separate files.
+
+## Usage
+Run a command such as `instance list` to generate the directories. To add new instances and profiles to your config, create files called `<instance>.json` or `<profile>.json` in your config directory, under the `instances` and `profiles` directories. Then, put the JSON configuration for it inside that new file.
---
+++ plugins/plugins/config_split/main.rs
@@ -0,0 +1,64 @@
+use std::{
+	collections::HashMap,
+	path::{Path, PathBuf},
+	sync::Arc,
+};
+
+use anyhow::Context;
+use mcvm_core::io::json_from_file;
+use mcvm_plugin::api::CustomPlugin;
+use serde::de::DeserializeOwned;
+
+fn main() -> anyhow::Result<()> {
+	let mut plugin = CustomPlugin::from_manifest_file("config_split", include_str!("plugin.json"))?;
+
+	plugin.add_instances(|_, arg| {
+		let config_dir = PathBuf::from(arg.config_dir);
+		let dir = config_dir.join("instances");
+		if !dir.exists() {
+			let _ = std::fs::create_dir_all(&dir);
+		}
+
+		get_config_files(&dir)
+	})?;
+	plugin.add_profiles(|_, arg| {
+		let config_dir = PathBuf::from(arg.config_dir);
+		let dir = config_dir.join("profiles");
+		if !dir.exists() {
+			let _ = std::fs::create_dir_all(&dir);
+		}
+
+		get_config_files(&dir)
+	})?;
+
+	Ok(())
+}
+
+/// Gets config files from the given directory
+fn get_config_files<D: DeserializeOwned>(directory: &Path) -> anyhow::Result<HashMap<Arc<str>, D>> {
+	let reader = directory.read_dir().context("Failed to read directory")?;
+
+	let mut out = HashMap::with_capacity(reader.size_hint().0);
+	for entry in reader {
+		let entry = entry.context("Failed to read directory entry")?;
+		if entry
+			.file_type()
+			.context("Failed to get entry file type")?
+			.is_dir()
+		{
+			continue;
+		}
+
+		let name = entry.file_name().to_string_lossy().to_string();
+		if !name.ends_with(".json") {
+			continue;
+		}
+		let name = &name[0..name.len() - 6];
+
+		let contents = json_from_file(entry.path())
+			.with_context(|| format!("Failed to read config file '{name}'"))?;
+		out.insert(Arc::from(name), contents);
+	}
+
+	Ok(out)
+}
