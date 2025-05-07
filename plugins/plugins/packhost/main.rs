81bd342146e24c370914b2bfb939c6838cb27e70
CarbonSmasher <crabonsmasher@gmail.com>
2025-05-07 12:06:13 -0400

    feat(packhost): More work

--- plugins/plugins/packhost/main.rs
+++ plugins/plugins/packhost/main.rs
@@ -1,25 +1,34 @@
+use std::fs::File;
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+use std::path::PathBuf;
 
 use anyhow::Context;
 use mcvm_plugin::api::CustomPlugin;
+use serde::Deserialize;
 
 fn main() -> anyhow::Result<()> {
 	let mut plugin = CustomPlugin::from_manifest_file("packhost", include_str!("plugin.json"))?;
 	plugin.while_instance_launch(|_, arg| {
-		let path = arg.config.common.plugin_config.get("hosted_resource_pack");
-		let Some(path) = path else {
+		let config = arg.config.common.plugin_config.get("packhost");
+		let Some(config) = config else {
 			return Ok(());
 		};
-		let _path: String = serde_json::from_value(path.clone())
-			.context("Failed to read hosted resource pack path")?;
+		let config: Config = serde_json::from_value(config.clone())
+			.context("Failed to read hosted resource pack config")?;
 
-		let listener = TcpListener::bind("127.0.0.1:80").context("Failed to bind TCP listener")?;
+		let listener = TcpListener::bind(format!("127.0.0.1:{}", config.port.unwrap_or(8080)))
+			.context("Failed to bind TCP listener")?;
+
+		// Figure out the hash of the file
+		let path = PathBuf::from(config.path);
+		let mut file =
+			BufReader::new(File::open(path).context("Failed to open resource pack file")?);
 
 		for stream in listener.incoming() {
 			let stream = stream.unwrap();
 
-			handle_connection(stream);
+			let _ = handle_connection(stream, &mut file);
 		}
 
 		Ok(())
@@ -28,7 +37,7 @@ fn main() -> anyhow::Result<()> {
 	Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream, _file: &mut BufReader<File>) -> anyhow::Result<()> {
 	let buf_reader = BufReader::new(&stream);
 	let _: Vec<_> = buf_reader
 		.lines()
@@ -36,7 +45,21 @@ fn handle_connection(mut stream: TcpStream) {
 		.take_while(|line| !line.is_empty())
 		.collect();
 
-	let response = "HTTP/1.1 200 OK\r\n\r\n";
+	let status_line = "HTTP/1.1 200 OK\r\n";
+	let headers = "Content-Type:application/zip\r\n";
 
-	stream.write_all(response.as_bytes()).unwrap();
+	stream
+		.write_all(status_line.as_bytes())
+		.context("Failed to write status line")?;
+	stream
+		.write_all(headers.as_bytes())
+		.context("Failed to write headers")?;
+
+	Ok(())
+}
+
+#[derive(Deserialize)]
+struct Config {
+	pub path: String,
+	pub port: Option<u16>,
 }
